10e473ee13a271b0756f23d5855608216f5d7594
GROX <103297099+g4o4ox@users.noreply.github.com>
2026-06-15 22:39:41 -0300

    Reescrevendo o Bootloader
    Não finalizado, usando o guia do DevOS 
    > https://wiki.osdev.org/Bare_Bones_with_NASM

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,1 +1,25 @@
 
+; Iniciando fazendo movimentação de bits
+MBALIGIN equ 1<<0  ; move 0 1 byte  a esquerda
+MEMINFO	equ 1<<1 ; move 1 1 byte a esquerda
+MBFLAGS equ MBALIGN | MEMINFO ; 0101 /
+MAGICNUMBER equ 0x1BADB002
+
+; definindo as variaveis
+
+section .multiboot ; função do multiboot
+align 4 ; alinhar 4 bytes
+	dd MAGIC
+	dd MBFLAGS
+	dd ;
+
+
+section .idontunderstandkkkkkkk
+align 16 ; alinhar 16bits  ; move os dados para o fim da stack
+stack_bottom:
+resb 16384 ; 16KiB
+stack_top:
+
+section .msg
+global _start:function (_start.end - _start)
+_start:
