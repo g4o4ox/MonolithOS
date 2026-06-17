e8fa5dfe0809fa06fce57d955eaf024513a48423
GROX <103297099+g4o4ox@users.noreply.github.com>
2026-06-16 21:08:30 -0300

    Fix assembly constants and section definitions

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,20 +1,20 @@
 
 ; Iniciando fazendo movimentação de bits
-MBALIGIN equ 1<<0  ; move 0 1 byte  a esquerda
+MBALIGN equ 1<<0  ; move 0 1 byte  a esquerda
 MEMINFO	equ 1<<1 ; move 1 1 byte a esquerda
 MBFLAGS equ MBALIGN | MEMINFO ; 0101 /
 MAGICNUMBER equ 0x1BADB002
-
+CHECKSUM equ -(MAGICNUMBER + MBFLAGS)
 ; definindo as variaveis
 
 section .multiboot ; função do multiboot
 align 4 ; alinhar 4 bytes
-	dd MAGIC
+	dd MAGICNUMBER
 	dd MBFLAGS
-	dd ;
+	dd CHECKSUM;
 
 
-section .idontunderstandkkkkkkk
+section .bss
 align 16 ; alinhar 16bits  ; move os dados para o fim da stack
 stack_bottom:
 resb 16384 ; 16KiB
@@ -23,3 +23,11 @@ stack_top:
 section .msg
 global _start:function (_start.end - _start)
 _start:
+	mov esp, stack_top
+
+	extern kernel_main 
+	call kernel_main ; chama o codigo externo do kernel
+	cli
+.hang: hlt
+	jmp .hang
+.end:
